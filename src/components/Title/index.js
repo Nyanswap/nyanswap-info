eb0d8efaa351f8241d211ff2ebec1961026293af
catnip <catnip@nyanswap.com>
2020-10-23 17:48:58 +0900

    update

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -5,8 +5,8 @@ import styled from 'styled-components'
 import { Flex } from 'rebass'
 import Link from '../Link'
 import { RowFixed } from '../Row'
-import Logo from '../../assets/logo_white.svg'
-import Wordmark from '../../assets/wordmark_white.svg'
+import Logo from '../../assets/logo_nyan.svg'
+import Wordmark from '../../assets/logo_typeonly_color.svg'
 
 const TitleWrapper = styled.div`
   text-decoration: none;
